356e40d4034216486b754d1cbfc923e31d54c940
zk8080 <zhangkang8080@163.com>
2022-08-28 13:08:33 +0800

    chore: 图片及svg静态资源处理

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
+import logoSrc from "@assets/react.svg";
+import { ReactComponent as ReactLogo } from "@assets/react.svg";
+
 function Index() {
 	return (
-		<div className="bg-blue-400">
+		<div>
 			<h1 className="text-3xl font-bold underline bg-blue-400 text-red-500">
 				首页
 			</h1>
+			<img src={logoSrc} alt="" />
+			<ReactLogo></ReactLogo>
 		</div>
 	);
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
 import viteEslint from "vite-plugin-eslint";
 import viteStylelint from "vite-plugin-stylelint";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), viteEslint(), viteStylelint()]
+	plugins: [react(), viteEslint(), viteStylelint(), svgr()],
+	resolve: {
+		alias: {
+			"@assets": path.join(__dirname, "./src/assets")
+		}
+	}
 });
